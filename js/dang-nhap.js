727f5ab1e16cb734c7ba3780eed69362a691100f
Vincent Van Gogh <khangmuahe@gmail.com>
2023-02-02 22:58:52 +0700

    02 02 2023

--- js/dang-nhap.js
+++ js/dang-nhap.js
@@ -7,9 +7,10 @@ let check = document.querySelector(".check");
 btn.addEventListener("click", function (e) {
   let _userName = document.querySelector(".username").value;
   let _passWord = document.querySelector(".password").value;
+  console.log(_userName,_passWord);
   for (let i = 0; i < test.length; i++) {
-    if (test[i].tenDangNhap == _userName && test[i].matKhau == _passWord) {
-        link.href = "homepage.html";
+    if (test[i].tenDangNhap == _userName  && test[i].matKhau == _passWord) {
+        window.location.replace("homepage.html");
         let now = {
           userName: _userName,
           password: _passWord
@@ -18,6 +19,7 @@ btn.addEventListener("click", function (e) {
     }
     else{
       check.innerHTML = "<p>Bạn đã nhập sai tài khoản hoặc mật khẩu</p>"
+      e.preventDefault();
       
     }
   }
--- js/nav-bar.js
+++ js/nav-bar.js
@@ -10,19 +10,32 @@ let arr = [
   {
     a: "product.html",
     name: " Sản phẩm ",
-  },{
-    a:"contact.html",
-    name:"Liên hệ"
-  },
+  }
 ];
+let arr1 = {
+  a:"login.html",
+  name : "Đăng nhập"
+}
 
 let navBar = document.querySelector(".nav-bar");
 if(navBar){
     navBar.innerHTML = '';
     for(let i of arr){
-        navBar.innerHTML +=`     <div class="link">
-        <a href="/${i.a}">${i.name}</a>
-    </div>
-    `
+        navBar.innerHTML +=`<div class="link">
+                            <a href="/${i.a}">${i.name}</a>
+                            </div>
+                            `
     }
+}
+let login = document.querySelector(".login");
+let tk = JSON.parse(localStorage.getItem("now"));
+if(tk != null){
+  login.innerHTML = `Xin chào ${tk.userName}
+                  <button onclick="signOut()">Đăng xuất</button>`;
+}else if(tk == null){
+  login.innerHTML =` <a href="/${arr1.a}">${arr1.name}</a>`
+}
+function signOut(){
+  localStorage.removeItem("now");
+  login.innerHTML = `<a href="/login.html">Đăng nhập</a>`
 }
